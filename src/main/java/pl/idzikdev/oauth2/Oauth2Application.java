dc4e34b4333687fffad41e566b5ea1d0181d92d8
Tomasz Idzikowski <idzikdev@gmail.com>
2019-03-26 22:27:37 +0100

    do poprawki

--- src/main/java/pl/idzikdev/oauth2/Oauth2Application.java
+++ src/main/java/pl/idzikdev/oauth2/Oauth2Application.java
@@ -2,12 +2,21 @@ package pl.idzikdev.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Oauth2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2Application.class, args);
+	}
+
+	@Bean
+	public String getPassword(){
+		BCryptPasswordEncoder bCryptPasswordEncoder=new BCryptPasswordEncoder();
+		System.out.println(bCryptPasswordEncoder.encode("user"));
+		return bCryptPasswordEncoder.encode("user");
 	}
 
 }
--- src/main/java/pl/idzikdev/oauth2/config/OAuthConfiguration.java
+++ src/main/java/pl/idzikdev/oauth2/config/OAuthConfiguration.java
@@ -1,20 +1,61 @@
 package pl.idzikdev.oauth2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import pl.idzikdev.oauth2.services.UserService;
 
 @Configuration
 @EnableAuthorizationServer
 public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
 
     private AuthenticationManager manager;
+    private UserDetailsService service;
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.tokenKeyAccess("permitAll()")
                 .checkTokenAccess("isAuthenticated()");
     }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("clientid")
+                .secret("secret")
+                .authorizedGrantTypes("password","authorization_code","refresh_token")
+                .scopes("read","write")
+                .autoApprove(true);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .tokenStore(tokenStore())
+                .authenticationManager(manager)
+                .accessTokenConverter(accessTokenConverter());
+//                .userDetailsService(service);
+    }
+    @Bean
+    public TokenStore tokenStore(){
+        return new JwtTokenStore(accessTokenConverter());
+    }
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter converter=new JwtAccessTokenConverter();
+        converter.setSigningKey("123456");
+        return converter;
+    }
+
+
 }
